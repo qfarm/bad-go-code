e7118589d1b07eee36ee684d22b5b9255581d5ba
Paweł Kowalak <pawel.kowalak@gmail.com>
2016-03-19 01:57:10 +0100

    Coverage example

---
+++ cover/b_test.go
@@ -0,0 +1,8 @@
+package cover
+
+import "testing"
+
+func TestB(t *testing.T) {
+	b(0)
+	b(1)
+}
---
+++ cover/dir2/two.go
@@ -0,0 +1,8 @@
+package two
+
+func a(i int) bool {
+	if i > 0 {
+		return true
+	}
+	return false
+}
